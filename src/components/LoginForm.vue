982cddd0681a108a0a3a1fedf8c9538e267c0ff6
PepikXD <stepan.nacelnik@gmail.com>
2024-12-08 19:48:37 +0100

    pepek

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="form-example">
+    <div class="form-example">
+      <label>Enter your name: </label>
+      <input type="text" v-model="name"/>
+    </div>
+    <div class="form-example">
+      <label>Enter your email: </label>
+      <input type="email" v-model="email"/>
+    </div>
+    <div class="form-example">
+      <button>Submit</button>
+    </div>
+    <hr>
+    <div>
+      <label>Your name is: {{ name }}</label>
+    </div>
+    <div>
+      <label>Your email is: {{ email }}</label>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let name = ref("")
+let email = ref("")
+
+</script>
+
+<style scoped>
+
+</style>
